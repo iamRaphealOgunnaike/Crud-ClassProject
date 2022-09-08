f7540cbf4ed3abe02a6047ac6dbec5e80ec125f2
ogunraph_lab <scitech4africa@gmail.com>
2022-09-08 10:03:45 -0700

    update

--- authInMern/server/model/user.js
+++ authInMern/server/model/user.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
-const jwt = require('jsonwebtoken ')
+const jwt = require('jsonwebtoken ');
+const joi = require('joi');
+const passwordComplexity = require('joi-password-complexity');
 
 const userSchema = new mongoose.Schema({
     firstName:{type:String, require: true},
@@ -8,5 +10,22 @@ const userSchema = new mongoose.Schema({
 });
 
 userSchema.methods.generateAuthToToken = function (){
-    const token =jwt.sign({id: this_id}, process.env.jwt)
-}
+    const token = jwt.sign({_id: this_id}, process.env.JWTPRIVATEKEY, {expiresIn:"7d"})
+    return token 
+};
+
+
+const User = mongoose.model("user", userSchema);
+
+const validate =(data)=>{
+    const schema = joi.object({
+        firstName:joi.string().required().label('Firstname'),
+        lastName:joi.string().required().label('Lastname'),
+        email:joi.string().required().label('Email'),
+        password:joi.passwordComplexity().required().label('Password'),
+    });
+    return schema.validate(data)
+};
+
+
+module.exports ={User,validate}
